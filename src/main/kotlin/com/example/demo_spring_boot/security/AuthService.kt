c7041c151d8d9cdbf79ffa07b106c1a3710d7089
Muhammad Haris Naeem <33431549+haris42747@users.noreply.github.com>
2025-08-17 00:30:59 +0500

    Response Status Code Added.

--- src/main/kotlin/com/example/demo_spring_boot/security/AuthService.kt
+++ src/main/kotlin/com/example/demo_spring_boot/security/AuthService.kt
@@ -5,9 +5,11 @@ import com.example.demo_spring_boot.database.model.User
 import com.example.demo_spring_boot.database.repository.RefreshTokenRepository
 import com.example.demo_spring_boot.database.repository.UserRepository
 import org.bson.types.ObjectId
+import org.springframework.http.HttpStatusCode
 import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.server.ResponseStatusException
 import java.security.MessageDigest
 import java.time.Instant
 import java.util.*
@@ -50,17 +52,17 @@ class AuthService(
     @Transactional
     fun refresh(refreshToken: String): TokenPair {
         if (!jwtService.validateRefreshToken(refreshToken)) {
-            throw IllegalArgumentException("Invalid refresh token.")
+            throw ResponseStatusException(HttpStatusCode.valueOf(401),"Invalid refresh token.")
         }
 
         val userId = jwtService.getUserIdFromToken(refreshToken)
         val user = userRepository.findById(ObjectId(userId)).orElseThrow {
-            IllegalArgumentException("Invalid refresh token.")
+            ResponseStatusException(HttpStatusCode.valueOf(401),"Invalid refresh token.")
         }
 
         val hashed = hashToken(refreshToken)
         refreshTokenRepository.findByUserIdAndHashedToken(user.id, hashed)
-            ?: throw IllegalArgumentException("Refresh token not recognized (maybe used or expired?)")
+            ?: throw ResponseStatusException(HttpStatusCode.valueOf(401),"Refresh token not recognized (maybe used or expired?)")
 
         refreshTokenRepository.deleteByUserIdAndHashedToken(user.id, hashed)
 
--- src/main/kotlin/com/example/demo_spring_boot/security/JwtService.kt
+++ src/main/kotlin/com/example/demo_spring_boot/security/JwtService.kt
@@ -4,14 +4,16 @@ import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.security.Keys
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpStatusCode
 import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
 import java.util.Base64
 import java.util.Date
 
 @Service
-class JwtService (
+class JwtService(
     @Value("\${jwt.secret}") private val jwtSecret: String
-){
+) {
     private val secretKey = Keys.hmacShaKeyFor(Base64.getDecoder().decode(jwtSecret))
     private val accessTokenValidityMs = 15L * 60L * 1000L
     val refreshTokenValidityMs = 30L * 24L * 60L * 60L * 1000L
@@ -20,55 +22,58 @@ class JwtService (
         userId: String,
         type: String,
         expiry: Long
-    ): String{
+    ): String {
         val now = Date()
         val expiryDate = Date(now.time + expiry)
         return Jwts.builder()
             .subject(userId)
-            .claim("type",type)
+            .claim("type", type)
             .issuedAt(now)
             .expiration(expiryDate)
-            .signWith(secretKey,Jwts.SIG.HS256)
+            .signWith(secretKey, Jwts.SIG.HS256)
             .compact()
     }
 
-    fun generateAccessToken(userId:String): String{
-        return  generateToken(userId,"access",accessTokenValidityMs)
+    fun generateAccessToken(userId: String): String {
+        return generateToken(userId, "access", accessTokenValidityMs)
     }
 
-    fun generateRefreshToken(userId:String): String{
-        return  generateToken(userId,"refresh",refreshTokenValidityMs)
+    fun generateRefreshToken(userId: String): String {
+        return generateToken(userId, "refresh", refreshTokenValidityMs)
     }
 
-    fun validateAccessToken(token:String):Boolean{
-        val claims = parseAllClaims(token)?: return  false
-        val tokenType  = claims["type"] as? String ?: return false
+    fun validateAccessToken(token: String): Boolean {
+        val claims = parseAllClaims(token) ?: return false
+        val tokenType = claims["type"] as? String ?: return false
         return tokenType == "access"
     }
 
-    fun validateRefreshToken(token:String):Boolean{
-        val claims = parseAllClaims(token)?: return  false
-        val tokenType  = claims["type"] as? String ?: return false
+    fun validateRefreshToken(token: String): Boolean {
+        val claims = parseAllClaims(token) ?: return false
+        val tokenType = claims["type"] as? String ?: return false
         return tokenType == "refresh"
     }
 
     // Authorization : <token>
-    fun getUserIdFromToken(token: String): String{
-        val claims = parseAllClaims(token)?:throw IllegalArgumentException("Invalid Token")
+    fun getUserIdFromToken(token: String): String {
+        val claims = parseAllClaims(token) ?: throw ResponseStatusException(
+            HttpStatusCode.valueOf(401),
+            "Invalid Token"
+        )
         return claims.subject
     }
 
-    private fun parseAllClaims(token:String): Claims?{
-        val rawToken = if (token.startsWith("Bearer ")){
+    private fun parseAllClaims(token: String): Claims? {
+        val rawToken = if (token.startsWith("Bearer ")) {
             token.removePrefix("Bearer ")
-        }else token
-        return try{
+        } else token
+        return try {
             Jwts.parser()
                 .verifyWith(secretKey)
                 .build()
                 .parseSignedClaims(rawToken)
                 .payload
-        } catch (e:Exception){
+        } catch (e: Exception) {
             null
         }
     }
